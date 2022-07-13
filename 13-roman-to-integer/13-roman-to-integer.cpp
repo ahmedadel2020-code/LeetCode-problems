2f489845169c253282ad37bca608954c1d544cc7
Ahmed Adel <62815241+ahmedadel2020-code@users.noreply.github.com>
2022-07-13 16:27:54 +0200

    Time: 13 ms (66.05%), Space: 6 MB (64.25%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,29 +1,112 @@
 class Solution {
 public:
     int romanToInt(string s) {
-    unordered_map<char, int> Table =
+                    int sum = 0;
+    int I = 1;
+    int V = 5;
+    int X = 10;
+    int L = 50;
+    int C = 100;
+    int D = 500;
+    int M = 1000;
+    bool flag = false;
+
+    for (int i = 0; i < s.length(); i++)
     {
-        {'I', 1},
-        {'V', 5},
-        {'X', 10},
-        {'L', 50},
-        {'C', 100},
-        {'D', 500},
-        {'M', 1000}
-    };
-    int sum = 0;
-    for (int i = 0; i < s.length() - 1; i++)
-    {
-        if (Table[s[i]] < Table[s[i + 1]])
+        if (s[i] == 'I' && s[i + 1] != 'V' && s[i + 1] != 'X')
         {
-            sum -= Table[s[i]];
+            if (flag)
+            {
+                flag = false;
+                continue;
+            }
+            sum += I;
         }
-        else
+        else if (s[i] == 'V')
         {
-            sum += Table[s[i]];
+            if (flag)
+            {
+                flag = false;
+                continue;
+            }
+            sum += V;
+        }
+        else if (s[i] == 'X' && s[i + 1] != 'L' && s[i + 1] != 'C')
+        {
+            if (flag)
+            {
+                flag = false;
+                continue;
+            }
+            sum += X;
+        }
+        else if (s[i] == 'L')
+        {
+            if (flag)
+            {
+                flag = false;
+                continue;
+            }
+            sum += L;
+        }
+        else if (s[i] == 'C' && s[i + 1] != 'D' && s[i + 1] != 'M')
+        {
+            if (flag)
+            {
+                flag = false;
+                continue;
+            }
+            sum += C;
+        }
+        else if (s[i] == 'D')
+        {
+            if (flag)
+            {
+                flag = false;
+                continue;
+            }
+            sum += D;
+        }
+        else if (s[i] == 'M')
+        {
+            if (flag)
+            {
+                flag = false;
+                continue;
+            }
+            sum += M;
+        }
+        else if (s[i] == 'I' && s[i + 1] == 'V')
+        {
+            flag = true;
+            sum += 4;
+        }
+        else if (s[i] == 'I' && s[i + 1] == 'X')
+        {
+            flag = true;
+            sum += 9;
+        }
+        else if (s[i] == 'X' && s[i + 1] == 'L')
+        {
+            flag = true;
+            sum += 40;
+        }
+        else if (s[i] == 'X' && s[i + 1] == 'C')
+        {
+            flag = true;
+            sum += 90;
+        }
+        else if (s[i] == 'C' && s[i + 1] == 'D')
+        {
+            flag = true;
+            sum += 400;
+        }
+        else if (s[i] == 'C' && s[i + 1] == 'M')
+        {
+            flag = true;
+            sum += 900;
         }
     }
-    sum += Table[s.back()];
     return sum;
     }
 };
