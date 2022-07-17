8988569e1e11476193ed0c122be5c3b8f3fd616b
Ahmed Adel <62815241+ahmedadel2020-code@users.noreply.github.com>
2022-07-17 09:10:35 +0200

    Time: 3 ms (53.86%), Space: 6.2 MB (88.76%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,49 +1,14 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        vector<int> vt;
-        if (s.empty())
+        int idx = 0;
+        for (int i = 0; i < t.length() and idx < s.length(); i++)
         {
-            return true;
-        }
-        int pos = 0;
-        if (s[0] == t[0])
-        {
-            vt.push_back(0);
-        }
-        for (int i = 0; i < s.length(); i++)
-        {
-            for (int j = pos + 1; j < t.length(); j++)
+            if (s[idx] == t[i])
             {
-                if (s[i] == t[j])
-                {
-                    pos = j;
-                    vt.push_back(pos);
-                    break;
-                }
+                idx++;
             }
         }
-        if (!vt.empty())
-        {
-            int curr = vt[0];
-            for (int i = 0; i < vt.size(); i++)
-            {
-                if (vt[i] < curr)
-                {
-                    return false;
-                }
-                else
-                {
-                    curr = vt[i];
-                }
-            }
-        }
-        if (vt.size() < s.size())
-        {
-            return false;
-        }
-
-
-        return true;
+        return (idx == s.length());
     }
 };
