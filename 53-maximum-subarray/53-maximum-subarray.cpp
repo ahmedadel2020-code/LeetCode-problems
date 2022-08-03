5a6a0b17404efe669c85ff3d48329019cf1221e9
Ahmed Adel <62815241+ahmedadel2020-code@users.noreply.github.com>
2022-08-03 14:21:33 +0200

    Time: 160 ms (60.43%), Space: 67.7 MB (54.07%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       int max_end = 0;
+        int max_so_far = INT_MIN;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            max_end += nums[i];
+            if (max_so_far < max_end)
+            {
+                max_so_far = max_end;
+            }
+            if (max_end < 0)
+            {
+                max_end = 0;
+            }
+        }
+        return max_so_far;
+    }
+};
